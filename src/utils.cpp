03671fb75185ab4a18266fe37fd2d1667da2a313
Sen Macmaster <senmac1000@gmail.com>
2024-08-16 15:16:16 +1200

    Utilities classes for cross-platform

---
+++ src/utils.cpp
@@ -0,0 +1,20 @@
+#include "utils.h"
+#include <cstdlib> 
+
+// sleep function delays next command (by x seconds)
+void sleep(short seconds) {
+    #ifdef __APPLE__
+        sleep(seconds);
+    #else
+        Sleep(second * 1000); // converted from milliseconds
+    #endif
+}
+
+// clear function removes all text on terminal
+void clear(){
+    #ifdef __APPLE__
+        system("clear");
+    #else
+        system("cls");
+    #endif
+}
