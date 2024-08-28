56c4687f96d791401d7ae481ab389f0d63a6ead7
Hayden Aish <aisha876@student.otago.ac.nz>
2024-08-28 20:38:17 +1200

    working movement in game loop

--- src/dungeon_dreams/main.cpp
+++ src/dungeon_dreams/main.cpp
@@ -13,6 +13,9 @@
 #include <iostream>
 using namespace std;
 
+short dungeon_sizes[8][2] = {{3, 3}, {6, 5}, {6, 3}, {6, 5}, {10, 5}, {8, 7}, {8, 7}, {10, 9}}; // sizes of each dungeon level
+const int levels = 8;
+
 int main()
 {
     bool gameRunning = true;
@@ -21,16 +24,18 @@ int main()
 
     displayWelcome();
 
+    while (gameRunning)
+    {
+        resetPlayerStats();
 
-    while (gameRunning) {
-        resetPlayerStats();  
-        
         int countdown = 5;
         int position = 0;
 
-        while (countdown > 0) {
+        while (countdown > 0)
+        {
             system("clear");
-            for (int i = 0; i < position; i++) {
+            for (int i = 0; i < position; i++)
+            {
                 cout << endl;
             }
             cout << AsciiArt::dungeonEntranceArt << endl;
@@ -44,30 +49,38 @@ int main()
         cout << "You have entered the dungeon..." << endl;
         ::sleep(1);
 
-        for (int level = 1; level <= 8; level++) { 
-            setupLevel(level);  
+        for (int level = 1; level <= levels; level++)
+        {
+            setupLevel(level);
 
-            for(int move = 1; move <= 10; move++){
-                //gameCompleted = runMovement();  
+            // for (int i = 0; i < levels; i++)
+            // {
+            fillDungeon(dungeon_sizes[level - 1][0], dungeon_sizes[level - 1][1]);
+            generateDynamicLevels();
+            updateBoard();
+            int count = 0;
 
-                cout << "Move: " << move << endl;
-
-                int tileMovedTo = rand() % 4;  
-                if (tileMovedTo == 3){
-                    tileMovedTo = 0;
+            while (count < 5)
+            {
+                if (enterUserInput())
+                {
+                    updateBoard();
+                    short tyle = startEvent();
+                    // cout << tyle << endl;
+                    levelPlay(tyle);
+                    count++;
                 }
-                levelPlay(tileMovedTo); 
-                sleep(1);
             }
+            // }
             sleep(1);
-            cout << "You have completed level " << level << ". Press enter for next level...";
-            cin.ignore();  // Ignore any leftover characters in the input buffer
-            cin.get();     // Wait for user input
+            cout << "You have completed level " << level << ". Press enter for next level..." << endl;
+            cin.ignore(); // Ignore any leftover characters in the input buffer
+            cin.get();    // Wait for user input
         }
-            
+
         playerWon = true;
-        
+
         showEndScreen(playerWon);
-        gameRunning = false;  
+        gameRunning = false;
     }
 }
--- src/dungeon_dreams/movement.cpp
+++ src/dungeon_dreams/movement.cpp
@@ -1,46 +1,49 @@
+/**
+ * Movement.cpp
+ * 
+ * Created by: Sen Macmaster 
+ * Created on: 21/07/24
+ * 
+ * Last editted by: Sen Macmaster
+ * Last editted on: 26/08/24
+ */
+
 #include <iostream> 
 #include <cctype> 
 #include <cmath>
-#include <string> 
+#include <string>
 #include <time.h> 
 #include <vector>
 #include <utility>
+#include "movement.h"
 
 using namespace std; 
 
-// board config 
-string level_board =     "     |     |     \n" 
-
-                         "-----+-----+-----\n" 
-
-                         "     |     |     \n" 
-
-                         "-----+-----+-----\n" 
-
-                         "     |     |     \n"; 
-
+// Board config 
+string level_board; 
  
 const char player = 'X'; 
-short board_size = 0; 
 short y; 
 short x; 
 
- 
 // Room config
 const short length_of_room = 5; 
 const short height_of_room = 1; 
-const short length = 8; 
-const short height = 7; 
 
-short dynamic_dungeon[height][length]; 
-short level = 0; 
+vector<vector<short>> dynamic_dungeon; 
+
+short height;
+short length;
 
 // Positions of player 
 short movement; 
-short pos[2] = {static_cast<short>(height / 2), 0}; 
+short pos[2];
 
-// Console removal testing
-short lines_to_be_removed = 0; 
+short getLength() { return length; }
+short getHeight() { return height; }
+void setLength(short l) { length = l; }
+void setHeight(short h) { height = h; }
+
 
 // Check if a position is safe
 bool is_safe(int x, int y) {
@@ -118,184 +121,141 @@ void generate_rest() {
 
 }
 
-// Fill the dungeon with the generated path
-void fillDungeon() {
-    // Initialize the dungeon to zeros
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < length; j++) {
-            dynamic_dungeon[i][j] = 0;
-        }
+void fillDungeon(short l, short h) { 
+    setLength(l);
+    setHeight(h);
+
+    pos[0] = static_cast<short>(getHeight() / 2); 
+    pos[1] = 0; 
+
+    dynamic_dungeon.clear();
+    dynamic_dungeon.resize(getLength());
+    for(int i = 0; i < getLength(); i++) {
+        dynamic_dungeon[i].resize(getHeight(), 0);
     }
+
     // Use current time as seed for random generator 
     srand(time(0)); 
+    print_dungeon();
     // Generate the path
     generate_path();
     print_dungeon();
     generate_rest();
     print_dungeon();
-}
-
-void removeConsole(short remove){ 
-
-    string move_cursor_up = "\033[A"; 
-    string clear_line = "\033[2K"; 
-
-    for(int i = 0; i < remove; i++){ 
-        cout << move_cursor_up; 
-        cout << clear_line; 
-    } 
-    cout.flush(); 
 } 
 
-void generateDynamicLevels(short length, short height){ 
-
+void generateDynamicLevels() { 
+    
     string board; 
 
     x = length_of_room + 1; 
-    y = (x*length)*2; 
+    y = (x * getLength()) * 2; 
 
-    board_size++; 
-    for (int i = 0; i < height; i++) { 
-        for (int j = 0; j < (height_of_room * length); j++) { 
+    for (int i = 0; i < getHeight(); i++) { 
+        for (int j = 0; j < (height_of_room * getLength()); j++) { 
             board += string(length_of_room, ' '); 
-            if(j < length -1){ 
+            if(j < getLength() - 1){ 
                 board += "|"; 
             } 
         }
         board += "\n"; 
-        board_size++;
-        if (i < height - 1) { 
-            for (int j = 0; j < length; j++) { 
+        if (i < getHeight() - 1) { 
+            for (int j = 0; j < getLength(); j++) { 
                 board += string(length_of_room, '-'); 
-                if(j < length -1){ 
+                if(j < getLength() - 1){ 
                     board += "+"; 
                 } 
             } 
             board += "\n"; 
-            board_size++; 
         } 
     } 
     level_board = board; 
 } 
 
-void updateBoard(bool remove){ 
+void updateBoard(){ 
     movement = 2 + (pos[1] * x) + (pos[0] * y); 
-    level_board = level_board.replace(movement, 1 , 1, player);  // update board 
-    if(remove){ 
-        //removeConsole(board_size + lines_to_be_removed); 
-        lines_to_be_removed = 0; 
-    } 
+    level_board.replace(movement, 1 , 1, player);  // update board 
     cout << level_board << endl; 
 } 
 
 void clearBoard(){ 
     movement = 2 + (pos[1] * x) + (pos[0] * y); 
-    level_board = level_board.replace(movement, 1 , 1, ' ');  // update board 
+    level_board.replace(movement, 1 , 1, ' ');  // update board 
 } 
 
-bool isAgainstWall(char move){ 
+
+bool isAgainstWall(char move) { 
     // Vertical wall and horizontal wall checks 
-    if((move == 'W' && pos[0] <= 0) || (move == 'S' && pos[0] >= height-1)){ 
+    if((move == 'W' && pos[0] <= 0) || (move == 'S' && pos[0] >= getHeight() - 1)) { 
+        cout << "TEST" << endl;
         return true; 
-    }else if((move == 'A' && pos[1] <= 0) || (move == 'D' && pos[1] >= length-1)){ 
+    } else if((move == 'A' && pos[1] <= 0) || (move == 'D' && pos[1] >= getLength() - 1)) { 
+        cout << "TEST" << endl;
         return true; 
     } 
     return false; 
 } 
 
-void startEvent(){ 
-    short type_of_room; 
-    type_of_room = dynamic_dungeon[pos[0]][pos[1]]; 
-    switch(type_of_room){ 
-        case 1: 
-            cout << "SAFE" << endl; 
-            break; 
-        case 2: 
-            cout << "enemy" << endl; 
-            break; 
-        case 3: 
-            cout << "enemy" << endl; 
-            break; 
-        case 4: 
-            cout << "wall" << endl; 
-            break; 
-        default: 
-            cout << "Descend the dungeon" << endl; 
-            level++; 
-    } 
-    //lines_to_be_removed++; 
+short startEvent() { 
+    short type_of_room = dynamic_dungeon[pos[0]][pos[1]];
+    return type_of_room;
+    // switch(type_of_room) { 
+    //     case 0: 
+    //         cout << "SAFE" << endl; 
+    //         break; 
+    //     default: 
+    //         cout << "Descend the dungeon" << endl; 
+    // } 
 } 
 
-bool movementCheck(string input){ 
-    char direction; 
-    direction = toupper(input.at(0)); 
+bool movementCheck(string input) { 
+    char direction = toupper(input.at(0)); 
     clearBoard(); 
     if(direction == 'W' && !isAgainstWall('W')) { 
         pos[0] -= 1; 
-    }else if(direction == 'A' && !isAgainstWall('A')) { 
+    } else if(direction == 'A' && !isAgainstWall('A')) { 
         pos[1] -= 1; 
-    }else if (direction == 'S' && !isAgainstWall('S')) { 
+    } else if (direction == 'S' && !isAgainstWall('S')) { 
         pos[0] += 1; 
-    }else if (direction == 'D' && !isAgainstWall('D')) { 
+    } else if (direction == 'D' && !isAgainstWall('D')) { 
         pos[1] += 1; 
-    }else{ 
-        cout << "Could not make move,  try again" << endl; 
+    } else { 
+        cout << "Could not make move, try again" << endl; 
         return false; 
     } 
     return true; 
 } 
 
-bool enterUserInput(){ 
+bool enterUserInput() { 
     cout << "Enter movement WASD: "; 
-    lines_to_be_removed++; 
 
     string input; 
     cin >> input; 
 
-    if(input.length() == 1){ 
+    if(input.length() == 1) { 
         return movementCheck(input); 
     } 
-    if(input == "Help"){ 
+    if(input == "Help") { 
         cout << "--Help Menu--" << endl; 
-        lines_to_be_removed++; 
         return true; 
     } 
-    cout << "Could not make move,  try again" << endl; 
-    lines_to_be_removed++; 
+    cout << "Could not make move, try again" << endl; 
     return false; 
 }
 
-int main2() { 
-    cout << "Testing" << endl; 
+// int main() { 
+//     cout << "Testing" << endl; 
 
-    fillDungeon(); 
-    generateDynamicLevels(length, height); 
-    updateBoard(false); 
+//     fillDungeon(); 
+//     printVector();
+//     generateDynamicLevels(length, height); 
+//     updateBoard(); 
 
-    while(true){ 
-        if(enterUserInput()){ 
-            updateBoard(true); 
-            startEvent(); 
-        } 
-    }
-    return 0; 
-} 
-
-bool runMovement() {
-    fillDungeon();
-    generateDynamicLevels(length, height);
-    updateBoard(false);
-
-    while(true) {
-        if(enterUserInput()) {
-            updateBoard(true);
-            startEvent();
-        }
-
-        //this is where we will want to end the level, when the player reaches a certain positon on the board (i.e. gets through unharmed)
-    }
-
-    return true; 
-}
-
- 
+//     while(true) { 
+//         if(enterUserInput()) { 
+//             updateBoard(); 
+//             startEvent(); 
+//         } 
+//     }
+//     return 0; 
+// }
