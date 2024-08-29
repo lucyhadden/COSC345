aeb4262951b0382b8f2ececdbd9adc295bc6ad54
Lucy Hadden <lucyhadden12@gmail.com>
2024-08-29 14:06:22 +1200

    Added tests for start screen class and doxygen comments

--- Test/StartScreenTest.cpp
+++ Test/StartScreenTest.cpp
@@ -1,116 +1,79 @@
-// #define CATCH_CONFIG_MAIN
-// #include "catch.hpp"
-// #include <iostream>
-// #include <sstream>
-// #include <string>
-// #include <cstdlib>
-// #include <unistd.h>
+#include "gtest/gtest.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>  
+#include <unistd.h>
+#include "AsciiArt.h"
 
-// class AsciiArt {
-// public:
-//     static std::string dragonWelcomeArt;
-//     static std::string knightArt;
-//     static std::string mageArt;
-//     static std::string thiefArt;
-//     static std::string tankArt;
-//     static std::string clericArt;
-//     static std::string storyStartArt;
-// };
+// Dummy AsciiArt class for testing purposes
+class AsciiArt {
+public:
+    static inline std::string dragonWelcomeArt = "Dragon Art";
+    static inline std::string knightArt = "Knight Art";
+    static inline std::string mageArt = "Mage Art";
+    static inline std::string thiefArt = "Thief Art";
+    static inline std::string tankArt = "Tank Art";
+    static inline std::string clericArt = "Cleric Art";
+    static inline std::string storyStartArt = "Story Start Art";
+};
 
-// std::string AsciiArt::dragonWelcomeArt = "Welcome Dragon";
-// std::string AsciiArt::knightArt = "Knight Art";
-// std::string AsciiArt::mageArt = "Mage Art";
-// std::string AsciiArt::thiefArt = "Thief Art";
-// std::string AsciiArt::tankArt = "Tank Art";
-// std::string AsciiArt::clericArt = "Cleric Art";
-// std::string AsciiArt::storyStartArt = "Story Art";
+// Function prototypes (include the prototypes from the original code here)
 
+// Mock implementations to replace system-specific calls
+void mockSystemClear() {
+    // Do nothing
+}
 
-// extern void displayWelcome();
-// extern void displayMenu();
-// extern void handleMenuChoice();
-// extern void handleClassChoice();
-// extern void startingStory();
-// extern void pressAnyKeyToContinue2();
+void mockSleep(int seconds) {
+    // Do nothing
+}
 
+// Testing displayWelcome()
+TEST(DungeonDreamsTest, TestDisplayWelcome) {
+    // Expect the function to run without errors
+    EXPECT_NO_THROW(displayWelcome());
+}
 
-// TEST_CASE("Test displayWelcome", "[welcome]") {
-//     std::stringstream input("3\n");
-//     std::stringstream output;
+// Testing displayMenu()
+TEST(DungeonDreamsTest, TestDisplayMenu) {
+    // Redirect stdout to a stringstream to capture output
+    std::stringstream buffer;
+    std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
 
-//     std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf());
-//     std::streambuf* orig_cout = std::cout.rdbuf(output.rdbuf());
+    displayMenu();
 
-//     displayWelcome();
+    std::string output = buffer.str();
 
-//     std::cin.rdbuf(orig_cin);
-//     std::cout.rdbuf(orig_cout);
+    EXPECT_NE(output.find("Welcome to Dungeon Dreams"), std::string::npos);
+    EXPECT_NE(output.find("1. View Character Classes"), std::string::npos);
+    EXPECT_NE(output.find("2. Start Game"), std::string::npos);
+    EXPECT_NE(output.find("3. Exit"), std::string::npos);
 
-//     REQUIRE(output.str().find("Welcome Dragon") != std::string::npos);
-// }
+    // Restore the original stdout
+    std::cout.rdbuf(old);
+}
 
-// TEST_CASE("Test displayMenu and handleMenuChoice", "[menu]") {
-//     std::stringstream input("3\n");
-//     std::stringstream output;
+// Testing handleMenuChoice()
+TEST(DungeonDreamsTest, TestHandleMenuChoice) {
+    std::istringstream input("1\n");
+    std::cin.rdbuf(input.rdbuf());
 
-//     std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf());
-//     std::streambuf* orig_cout = std::cout.rdbuf(output.rdbuf());
+    EXPECT_NO_THROW(handleMenuChoice());
+}
 
-//     handleMenuChoice();
+// Testing displayCharacterClasses()
+TEST(DungeonDreamsTest, TestDisplayCharacterClasses) {
+    // Expect the function to run without errors
+    EXPECT_NO_THROW(displayCharacterClasses());
+}
 
-//     std::cin.rdbuf(orig_cin);
-//     std::cout.rdbuf(orig_cout);
+// Testing startingStory()
+TEST(DungeonDreamsTest, TestStartingStory) {
+    // Expect the function to run without errors
+    EXPECT_NO_THROW(startingStory());
+}
 
-//     REQUIRE(output.str().find("Welcome to Dungeon Dreams: The Goblin Guide!") != std::string::npos);
-//     REQUIRE(output.str().find("Exiting game. Goodbye!") != std::string::npos);
-// }
-
-// TEST_CASE("Test displayCharacterClasses", "[character_classes]") {
-//     std::stringstream input;
-//     std::stringstream output;
-
-//     std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf());
-//     std::streambuf* orig_cout = std::cout.rdbuf(output.rdbuf());
-
-//     displayCharacterClasses();
-
-//     std::cin.rdbuf(orig_cin);
-//     std::cout.rdbuf(orig_cout);
-
-//     REQUIRE(output.str().find("Knight Art") != std::string::npos);
-//     REQUIRE(output.str().find("Mage Art") != std::string::npos);
-//     REQUIRE(output.str().find("Thief Art") != std::string::npos);
-//     REQUIRE(output.str().find("Tank Art") != std::string::npos);
-//     REQUIRE(output.str().find("Cleric Art") != std::string::npos);
-// }
-
-// TEST_CASE("Test startingStory", "[story]") {
-//     std::stringstream input;
-//     std::stringstream output;
-
-//     std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf());
-//     std::streambuf* orig_cout = std::cout.rdbuf(output.rdbuf());
-
-//     startingStory();
-
-//     std::cin.rdbuf(orig_cin);
-//     std::cout.rdbuf(orig_cout);
-
-//     REQUIRE(output.str().find("Story Art") != std::string::npos);
-//     REQUIRE(output.str().find("One fateful morning, you set off for the capital, leaving behind your village and mundane life.") != std::string::npos);
-// }
-
-// TEST_CASE("Test pressAnyKeyToContinue2", "[user_interaction]") {
-//     std::stringstream input("\n");
-//     std::stringstream output;
-
-//     std::streambuf* orig_cin = std::cin.rdbuf(input.rdbuf());
-//     std::streambuf* orig_cout = std::cout.rdbuf(output.rdbuf());
-
-//     pressAnyKeyToContinue2();
-
-//     std::cin.rdbuf(orig_cin);
-//     std::cout.rdbuf(orig_cout);
-
-//     REQUIRE(output.str().find("Press enter to continue...") != std::string::npos);
-// }
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
