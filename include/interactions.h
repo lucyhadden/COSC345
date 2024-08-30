bb8638e9b94570e88aecf399ee5d08ee88a4a0e3
lizziestewarts <elizabeth.stewartnz@gmail.com>
2024-08-30 13:26:42 +1200

    edit interactions

--- include/interactions.h
+++ include/interactions.h
@@ -1,9 +1,21 @@
 #ifndef INTERACTIONS_H
 #define INTERACTIONS_H
 
+#include <string>
+#include "player.h"
+
+// Declare the global variables as extern
+extern std::string enemyType;
+extern short enemyHealth;
+extern short enemyDamage;
+extern std::string trapType;
+extern short trapDamage;
+
+// Declare global player object
+extern Player player;
+
 void levelPlay(int tileMovedTo);
 void setupLevel(int levelNumber);
-int getPlayerHealth();
 void resetPlayerStats();
 void pressAnyKeyToContinue(); 
 
