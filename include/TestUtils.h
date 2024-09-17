9fee5ed6668ce8f74362dd7e074d6bdd3c7f9c9e
Lucy Hadden <lucyhadden12@gmail.com>
2024-09-17 13:34:52 +1200

    working tests, fixed issues, build working

--- include/TestUtils.h
+++ include/TestUtils.h
@@ -1,13 +1,8 @@
 #ifndef TEST_UTILS_H
 #define TEST_UTILS_H
 
-// Shared mock implementations for tests
-inline void mockSystemClear() {
-    // Do nothing
-}
 
-inline void mockSleep(int seconds) {
-    // Do nothing
-}
+void mockSleep(int seconds);
+void mockSystemClear();
 
 #endif // TEST_UTILS_H
---
+++ src/dungeon_dreams/TestUtils.cpp
@@ -0,0 +1,12 @@
+#include "TestUtils.h"
+#include <iostream>
+
+// Mock system clear (do nothing in the test environment)
+void mockSystemClear() {
+    // No-op for testing purposes
+}
+
+// Mock sleep (do nothing in the test environment)
+void mockSleep(int seconds) {
+    // No-op for testing purposes
+}
