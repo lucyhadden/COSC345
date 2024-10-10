b0354e1288173eea4a7aeb4341fa679a75b4f6f0
lizziestewarts <elizabeth.stewartnz@gmail.com>
2024-10-10 20:26:51 +1300

    combining into one welcome test

--- Test/StartScreenTest.cpp
+++ Test/StartScreenTest.cpp
@@ -7,39 +7,82 @@
 #include "TestUtils.h"
 #include "utils.h"
 
-// Testing displayMenu()
-TEST(StartScreenTest, TestDisplayMenu) {
-    // Redirect stdout to a stringstream to capture output
-    std::stringstream buffer;
-    std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+TEST(StartScreenTest, Welcome) {
+    std::string expectedWelcome = R"(
+                         _                    _
+                  ,/                        \,
+        _________{(                          })_________
+       /.-------./\\                        //\.-------.\
+      //@@@@@@@//@@\\  )                (  //@@\\@@@@@@@\\
+     //@@@@@@@//@@@@>>/                  \<<@@@@\\@@@@@@@\\
+    //O@O@O@O//@O@O//                      \\O@O@\\O@O@O@O\\
+  //OOOOOOOO//OOOO||          \  /          ||OOOO\\OOOOOOOO\\
+ //O%O%O%O%//O%O%O%\\         ))((         //%O%O%O\\%O%O%O%O\\
+||%%%%%%%%//'  `%%%%\\       //  \\       //%%%%'   `\\%%%%%%%||
+((%%%%%%%((      %%%%%\\    ((    ))    //%%%%%       ))%%%%%%))
+ \:::' `::\\      `:::::\\   \)~~(/    //:::::'      //::' `:::/
+  )'     `;)'      (`  ` \\ `<@  @>' / / '  ')      `(;'     `(
+          (               \`\ )^^( /  /               )
+        _                  ) \\oo/   (
+       (@)                  \  `'   /                      _
+       |-|\__________________\__^__<________oOo__________ (@)
+       |-|                                  VVV           |-|
+       |-|   __          __  _                            |-|
+       |-|  \ \        / / | |                            |-|
+       |-|   \ \  /\  / /__| | ___ ___  _ __ ___   ___    |-|
+       |-|    \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \   |-|
+       |-|     \  /\  /  __/ | (_| (_) | | | | | |  __/   |-|
+       |-|      \/  \/ \___|_|\___\___/|_| |_| |_|\___|   |-|
+       |-|                                                |-|
+       |_|\_____________________________________________  | |
+       (@)                 / ,/ \_____/ \\ ~\/~         `\|-|
+        ~             ___//^~      \____/\\               (@)
+                     <<<  \     __  <____/||               ~
+                               <   \ <___/||
+                                  || <___//
+                                  \ \/__//
+                                   ~----~
+)";
+    std::string expectedStoryStart = R"(
+    _________________________________________________________
+    |                                                       |
+    |                                                       |
+    |   ^  ^  ^   ^      ___I_      ^  ^   ^  ^  ^   ^  ^   |
+    |  /|\/|\/|\ /|\    /\-_--\    /|\/|\ /|\/|\/|\ /|\/|\  |
+    |  /|\/|\/|\ /|\   /  \_-__\   /|\/|\ /|\/|\/|\ /|\/|\  |
+    |  /|\/|\/|\ /|\   |[]| [] |   /|\/|\ /|\/|\/|\ /|\/|\  |   
+    |                                                       |
+    |_______________________________________________________|
+)";
 
-    displayMenu();
-
-    std::string output = buffer.str();
-
-    EXPECT_NE(output.find("Welcome to Dungeon Dreams"), std::string::npos);
-    EXPECT_NE(output.find("1. View Character Classes"), std::string::npos);
-    EXPECT_NE(output.find("2. Start Game"), std::string::npos);
-    EXPECT_NE(output.find("3. Exit"), std::string::npos);
-
-    // Restore the original stdout
-    std::cout.rdbuf(old);
-}
-
-TEST(StartScreenTest, MenuChooseOption) {
     std::ostringstream outputBuffer;
     std::streambuf* oldCout = std::cout.rdbuf(outputBuffer.rdbuf()); 
 
     std::istringstream inputBuffer("99\n2\n"); // Simulate choosing the Knight class
     std::streambuf* oldCin = std::cin.rdbuf(inputBuffer.rdbuf());
 
-    handleMenuChoice();
+    displayWelcome();
 
     // Capture the output
     std::string outputStr = outputBuffer.str();
 
+    EXPECT_NE(outputStr.find(expectedWelcome), std::string::npos);
+
+    EXPECT_NE(outputStr.find("Welcome to Dungeon Dreams: The Goblin Guide!"), std::string::npos);
+    EXPECT_NE(outputStr.find("1. View Character Classes"), std::string::npos);
+    EXPECT_NE(outputStr.find("2. Start Game"), std::string::npos);
+    EXPECT_NE(outputStr.find("3. Exit"), std::string::npos);
+    EXPECT_NE(outputStr.find("Please enter your choice (1-3): "), std::string::npos);
+    
     EXPECT_NE(outputStr.find("Invalid choice. Please try again."), std::string::npos);
     EXPECT_NE(outputStr.find("Starting the game..."), std::string::npos);
+
+    EXPECT_NE(outputStr.find(expectedStoryStart), std::string::npos);
+    EXPECT_NE(outputStr.find("In a world filled with dungeons and monsters, adventure awaits at every corner. You are an aspiring adventurer, whose dreams have always been filled with epic quests and heroic deeds."), std::string::npos);
+    EXPECT_NE(outputStr.find("One fateful morning, you set off for the capital, leaving behind your village and mundane life. The grand walls of the capital greet you with an air of opportunity and excitement."), std::string::npos);
+    EXPECT_NE(outputStr.find("Are you ready to face the challenges ahead?"), std::string::npos);
+    EXPECT_NE(outputStr.find("With a heart full of hope and a spirit ready for adventure, you march straight to the Adventurers' Guild..."), std::string::npos);
+    EXPECT_NE(outputStr.find("Press enter to continue..."), std::string::npos);
 
     // Restore cout and cin
     std::cout.rdbuf(oldCout);
@@ -121,42 +164,6 @@ TEST(DisplayCharacterClassesTest, DisplayCharacterClassesOutput) {
     EXPECT_NE(output.find("5. Cleric"), std::string::npos);
     EXPECT_NE(output.find(expectedCleric), std::string::npos);
     EXPECT_NE(output.find("Press any key to return to the main menu..."), std::string::npos);
-
-    // Restore cout and cin
-    std::cout.rdbuf(oldCout);
-    std::cin.rdbuf(oldCin);
-}
-
-TEST(StartScreenTest, StartingStory) {
-    std::string expectedStoryStart = R"(
-    _________________________________________________________
-    |                                                       |
-    |                                                       |
-    |   ^  ^  ^   ^      ___I_      ^  ^   ^  ^  ^   ^  ^   |
-    |  /|\/|\/|\ /|\    /\-_--\    /|\/|\ /|\/|\/|\ /|\/|\  |
-    |  /|\/|\/|\ /|\   /  \_-__\   /|\/|\ /|\/|\/|\ /|\/|\  |
-    |  /|\/|\/|\ /|\   |[]| [] |   /|\/|\ /|\/|\/|\ /|\/|\  |   
-    |                                                       |
-    |_______________________________________________________|
-)";
-
-    std::ostringstream outputBuffer;
-    std::streambuf* oldCout = std::cout.rdbuf(outputBuffer.rdbuf()); 
-
-    std::istringstream inputBuffer("\n"); // Simulate choosing the Knight class
-    std::streambuf* oldCin = std::cin.rdbuf(inputBuffer.rdbuf());
-
-    startingStory();
-
-    // Capture the output
-    std::string outputStr = outputBuffer.str();
-
-    EXPECT_NE(outputStr.find(expectedStoryStart), std::string::npos);
-    EXPECT_NE(outputStr.find("In a world filled with dungeons and monsters, adventure awaits at every corner. You are an aspiring adventurer, whose dreams have always been filled with epic quests and heroic deeds."), std::string::npos);
-    EXPECT_NE(outputStr.find("One fateful morning, you set off for the capital, leaving behind your village and mundane life. The grand walls of the capital greet you with an air of opportunity and excitement."), std::string::npos);
-    EXPECT_NE(outputStr.find("Are you ready to face the challenges ahead?"), std::string::npos);
-    EXPECT_NE(outputStr.find("With a heart full of hope and a spirit ready for adventure, you march straight to the Adventurers' Guild..."), std::string::npos);
-    EXPECT_NE(outputStr.find("Press enter to continue..."), std::string::npos);
 
     // Restore cout and cin
     std::cout.rdbuf(oldCout);
